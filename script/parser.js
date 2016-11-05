57f4b00553fecc643aca378f81b592b9a5c4494a
Spriha811 <jha.spriha@gmail.com>
2016-11-05 13:05:28 -0500

    Updated

--- script/parser.js
+++ script/parser.js
@@ -1,5 +1,6 @@
 var apiKey = "&api_key=lnpVL48QKRvh2kyG4IpDzgCqv5cJQVU8pPsRKMLP";
 var urlRef = "https://api.nal.usda.gov/ndb/search/?format=json&q=";
+var urlRef2= "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=DEMO_KEY&nutrients=205&nutrients=203&nutrients=204&nutrients=208&nutrients=221&nutrients=601&nutrients=291&nutrients=269&nutrients=268"; 
 
 var sortByName = (sort) => "&sort=" + sort;
 var displayN = (num) => "&max=" + num;
@@ -10,11 +11,12 @@ var tmp; // use for testing, temporary variable
 var showN = 25;
 var page = 0;
 var stringResult;
+var nutrition = {};
 
 
 function search() {
     if (event.keyCode == 13) {
-    	
+
 		var url = urlRef + document.getElementById("searchBar").value + sortByName(document.getElementById("sortID").value) + displayN(showN) + offsetData(page) + apiKey;
 		console.log(url);
 		$.getJSON(url, function(data) {
@@ -62,5 +64,34 @@ function search() {
 
 function test(id) {
 	console.log(id);
-}
+	var url = urlRef2 + "&ndbno=" + id; 
+	console.log(url);
+	$.getJSON(url, function(data){
+		console.log(data);
+		tmp = data;
 
+			nutrition['item'] = [];
+			for (var i in data.report.foods) {
+				nutrition['item'].push({
+					name: data.report.foods[i].name,
+					ndbno: data.report.foods[i].ndbno,
+					weight: data.report.foods[i].weight,
+					measure: data.report.foods[i].measure,
+					nutrients: []
+				});
+				
+				for (var j in data.report.foods[i].nutrients) {
+					
+					nutrition['item'][i]['nutrients'].push({
+					id: data.report.foods[i].nutrients[j].nutrient_id,
+					nutrient: data.report.foods[i].nutrients[j].nutrient,
+					unit: data.report.foods[i].nutrients[j].unit,
+					gm: data.report.foods[i].nutrients[j].gm,
+					value: data.report.foods[i].nutrients[j].value
+				});	
+			}
+		
+			}
+	})
+
+}
